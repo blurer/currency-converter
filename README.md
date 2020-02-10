9221701affa3f08f51880cf809ff128647bf5368
Bryan Lurer <bl@nuc.lan>
2020-02-10 20:45:28 +0800

    cleaned up code

--- README.md
+++ README.md
@@ -2,20 +2,46 @@
 Python currency converter
 - All based on data from 09-Feb-2020.
 - No modules imported (yet).
+- Cleaned up the code quite a bit.
+- Default to USD if no input given.
+- Default to 1 if no input given.
 
-Usage:
-```
-# pi @ BL-Pi4 in ~/currency-converter on git:master o [9:40:20] C:126
-$ chmod +x convert.py
+Usage - no inputs
 
-# pi @ BL-Pi4 in ~/currency-converter on git:master x [9:40:24]
-$ ./convert.py
-Currency: USD
-How much: 1000
-JPY: 110000.0
-PHP: 50950.0
-JPY: 1500.0
-PHP: 71520.0
-GBP: 780.0
-EUR: 910.0
 ```
+bl@nuc:~/scripts/currency-converter$ ./convert.py 
+Currency [USD]: 
+How much [1]: 
+--------------------
+JPY: 110.0
+PHP: 50.95
+AUD: 1.5
+INR: 71.52
+GBP: 0.78
+EUR: 0.91
+```
+
+Usage - Various
+
+```
+bl@nuc:~/scripts/currency-converter$ ./convert.py 
+Currency [USD]: JPY
+How much [1]: 50000
+--------------------
+USD: 460.0
+PHP: 23000.0
+bl@nuc:~/scripts/currency-converter$ ./convert.py 
+Currency [USD]: EUR
+How much [1]: 12345
+--------------------
+GBP: 10493.25
+JPY: 1482757.95
+EUR: 13456.05
+```
+
+
+
+
+### TO DO:
+- Add in flags: -h (help), -c (currency), -a (amount), -t (date)
+
--- convert.py
+++ convert.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
-startingUnit = input('Currency: ')
-startingValue = input('How much: ')
-startingValue = float(startingValue)
+currency = input('Currency [USD]: ')
+startingValue = input('How much [1]: ')
+if startingValue == '':
+    startingValue = float(1)
+else:
+    startingValue = float(startingValue)
+
+if currency == '':
+    startingUnit = "USD"
+else:
+    startingUnit = str.upper(currency)
 print('--------------------')
 
 # usd to other
@@ -36,26 +44,31 @@ inr_usd = 0.014
 inr_eur = 0.013
 inr_gbp = 0.011
 
-if startingUnit == 'USD':
-    converted_USD_JPY = round(startingValue * usd_jpy, 2)
-    converted_USD_PHP = round(startingValue * usd_php, 2)
-    converted_USD_AUD = round(startingValue * usd_aud, 2)
-    converted_USD_EUR = round(startingValue * usd_eur, 2)
-    converted_USD_INR = round(startingValue * usd_inr, 2)
-    converted_USD_GBP = round(startingValue * usd_gbp, 2)
-    print('JPY:', converted_USD_JPY)
-    print('PHP:', converted_USD_PHP)
-    print('AUD:', converted_USD_AUD)
-    print('INR:', converted_USD_INR)
-    print('GBP:', converted_USD_GBP)
-    print('EUR:', converted_USD_EUR)
-elif startingUnit == 'usd':
-    converted_USD_JPY = round(startingValue * usd_jpy, 2)
-    converted_USD_PHP = round(startingValue * usd_php, 2)
-    converted_USD_AUD = round(startingValue * usd_aud, 2)
-    converted_USD_EUR = round(startingValue * usd_eur, 2)
-    converted_USD_INR = round(startingValue * usd_inr, 2)
-    converted_USD_GBP = round(startingValue * usd_gbp, 2)
+converted_USD_JPY = round(startingValue * usd_jpy, 2)
+converted_USD_PHP = round(startingValue * usd_php, 2)
+converted_USD_AUD = round(startingValue * usd_aud, 2)
+converted_USD_EUR = round(startingValue * usd_eur, 2)
+converted_USD_INR = round(startingValue * usd_inr, 2)
+converted_USD_GBP = round(startingValue * usd_gbp, 2)
+converted_JPY_USD = round(startingValue * jpy_usd, 2)
+converted_JPY_PHP = round(startingValue * jpy_php, 2)
+converted_PHP_USD = round(startingValue * php_usd, 2)
+converted_PHP_JPY = round(startingValue * php_jpy, 2)
+converted_EUR_USD = round(startingValue * eur_usd, 2)
+converted_EUR_JPY = round(startingValue * eur_jpy, 2)
+converted_EUR_GBP = round(startingValue * eur_gbp, 2)
+converted_GBP_USD = round(startingValue * gbp_usd, 2)
+converted_GBP_JPY = round(startingValue * gbp_jpy, 2)
+converted_GBP_EUR = round(startingValue * gbp_eur, 2)
+converted_INR_USD = round(startingValue * inr_usd, 2)
+converted_INR_EUR = round(startingValue * inr_eur, 2)
+converted_INR_GBP = round(startingValue * inr_gbp, 2)
+
+
+
+if startingUnit == '':
+    print('Error')
+elif startingUnit == 'USD':
     print('JPY:', converted_USD_JPY)
     print('PHP:', converted_USD_PHP)
     print('AUD:', converted_USD_AUD)
@@ -63,68 +76,23 @@ elif startingUnit == 'usd':
     print('GBP:', converted_USD_GBP)
     print('EUR:', converted_USD_EUR)
 elif startingUnit == 'JPY':
-    converted_JPY_USD = round(startingValue * jpy_usd, 2)
-    converted_JPY_PHP = round(startingValue * jpy_php, 2)
-    print('USD:', converted_JPY_USD)
-    print('PHP:', converted_JPY_PHP)
-elif startingUnit == 'jpy':
-    converted_JPY_USD = round(startingValue * jpy_usd, 2)
-    converted_JPY_PHP = round(startingValue * jpy_php, 2)
     print('USD:', converted_JPY_USD)
     print('PHP:', converted_JPY_PHP)
 elif startingUnit == 'PHP':
-    converted_PHP_USD = round(startingValue * php_usd, 2)
-    converted_PHP_JPY = round(startingValue * php_jpy, 2)
-    print('USD:', converted_PHP_USD)
-    print('JPY:', converted_PHP_JPY)
-elif startingUnit == 'php':
-    converted_PHP_USD = round(startingValue * php_usd, 2)
-    converted_PHP_JPY = round(startingValue * php_jpy, 2)
     print('USD:', converted_PHP_USD)
     print('JPY:', converted_PHP_JPY)
 elif startingUnit == 'EUR':
-    converted_EUR_USD = round(startingValue * eur_usd, 2)
-    converted_EUR_JPY = round(startingValue * eur_jpy, 2)
-    converted_EUR_GBP = round(startingValue * eur_gbp, 2)
-    print('GBP:', converted_EUR_GBP)
-    print('JPY:', converted_EUR_JPY)
-    print('EUR:', converted_EUR_USD)
-elif startingUnit == 'eur':
-    converted_EUR_USD = round(startingValue * eur_usd, 2)
-    converted_EUR_JPY = round(startingValue * eur_jpy, 2)
-    converted_EUR_GBP = round(startingValue * eur_gbp, 2)
     print('GBP:', converted_EUR_GBP)
     print('JPY:', converted_EUR_JPY)
     print('EUR:', converted_EUR_USD)
 elif startingUnit == 'GBP':
-    converted_GBP_USD = round(startingValue * gbp_usd, 2)
-    converted_GBP_JPY = round(startingValue * gbp_jpy, 2)
-    converted_GBP_EUR = round(startingValue * gbp_eur, 2)
-    print('USD:', converted_GBP_USD)
-    print('JPY:', converted_GBP_JPY)
-    print('EUR:', converted_GBP_EUR)
-elif startingUnit == 'gbp':
-    converted_GBP_USD = round(startingValue * gbp_usd, 2)
-    converted_GBP_JPY = round(startingValue * gbp_jpy, 2)
-    converted_GBP_EUR = round(startingValue * gbp_eur, 2)
     print('USD:', converted_GBP_USD)
     print('JPY:', converted_GBP_JPY)
     print('EUR:', converted_GBP_EUR)
 elif startingUnit == 'INR':
-    converted_INR_USD = round(startingValue * inr_usd, 2)
-    converted_INR_EUR = round(startingValue * inr_eur, 2)
-    converted_INR_GBP = round(startingValue * inr_gbp, 2)
-    print('USD:', converted_INR_USD)
-    print('EUR:', converted_INR_EUR)
-    print('GBP:', converted_INR_GBP)
-elif startingUnit == 'inr':
-    converted_INR_USD = round(startingValue * inr_usd, 2)
-    converted_INR_EUR = round(startingValue * inr_eur, 2)
-    converted_INR_GBP = round(startingValue * inr_gbp, 2)
     print('USD:', converted_INR_USD)
     print('EUR:', converted_INR_EUR)
     print('GBP:', converted_INR_GBP)
 else:
     print('Error. Try another currency.')
     print('Use: USD, JPY, GBP, EUR, PHP.')
-
